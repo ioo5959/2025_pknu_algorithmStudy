ba10c9d3e86f247e7239a2c1960140ffcf3532b1
onegqueen <rkdwldnjs30@naver.com>
2025-02-16 22:09:16 +0900

    1번/15분

---
+++ 1주차/1번_BOJ11053/1번_강지원.py
@@ -0,0 +1,17 @@
+import sys
+
+N = int(sys.stdin.readline())
+nums = list(map(int,sys.stdin.readline().split()))
+
+dp = []
+
+for i in range(N):
+    dp.append(1)
+    for j in range(i):
+        if nums[j]<nums[i]:
+            dp[i]=max(dp[i],dp[j]+1)
+    print(dp)
+
+          
+print(max(dp))
+
